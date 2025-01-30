3bdcaec199bb45d3eb26b7adf4e7bcf230d0152f
TimoshaPro <timashkovng@mail.ru>
2025-01-30 22:26:07 +0300

    код для считывания текста с изоброжения готов (правда я его сделал по гайду с ютуба и нечерта не понимаю как он работает но я магу им пользоватся и это главное)

---
+++ Untitled-1.py
@@ -0,0 +1,17 @@
+import easyocr
+
+
+def text_recognition(file_path):
+    reader = easyocr.Reader(["ru", "en"])
+    result = reader.readtext(file_path, detail=0)
+
+    return result
+
+
+def main():
+    file_path = input("Enter a file path: ")
+    print(text_recognition(file_path=file_path))
+
+
+if __name__ == "__main__": 
+    main()
