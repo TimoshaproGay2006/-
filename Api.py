7136fdb2c6b2128ca6fd26f1ee4a272cde8884e6
TimoshaPro <timashkovng@mail.ru>
2025-02-17 20:35:12 +0300

    итоговая версия с вправками в коде указано место куда на

--- Api.py
+++ Api.py
@@ -17,7 +17,7 @@ templates = Jinja2Templates(directory="templates")
 def ocr_image_to_json(image_file):
     try:
         image = Image.open(image_file.file)
-        pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+        pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe' #<-----
         data = pytesseract.image_to_data(image, output_type=pytesseract.Output.DICT, lang='rus+eng')
 
         json_data = []
@@ -69,7 +69,9 @@ async def get_json_file():
     json_filename = "output.json"
     if os.path.exists(json_filename):
         response = FileResponse(json_filename)
-        # Запланируем удаление файла после отправки
+        
+
+
         asyncio.get_event_loop().call_later(0.1, os.remove, json_filename)
         return response
     else:
